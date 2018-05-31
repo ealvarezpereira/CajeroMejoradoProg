b0f8305bf9d1adce7e09726338caabe0e9786695
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-05-31 13:33:12 +0200

    Iniciacion del programa

--- src/cajeromejorado/CajeroMejorado.java
+++ src/cajeromejorado/CajeroMejorado.java
@@ -5,6 +5,8 @@
  */
 package cajeromejorado;
 
+import com.cajero.vista.InterfazInicioSesion;
+
 /**
  *
  * @author quique
@@ -15,7 +17,8 @@ public class CajeroMejorado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InterfazInicioSesion ses = new InterfazInicioSesion();
+        ses.setVisible(true);
     }
     
 }
