9a34592fc8aaeab6aebf708dd6cebc75ac4f16b1
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-05-31 13:34:05 +0200

    Añadido metodo de conexion a base de datos.

--- src/com/cajero/modelo/Modelo.java
+++ src/com/cajero/modelo/Modelo.java
@@ -5,10 +5,21 @@
  */
 package com.cajero.modelo;
 
+import com.cajero.libreria.ConexionesBD;
+
 /**
  *
  * @author quique
  */
 public class Modelo {
-    
+
+    ConexionesBD db = new ConexionesBD();
+
+    /**
+     * @param url Ruta sqlite de la base de datos.
+     */
+    public void conexionBD() {
+        String url = "jdbc:sqlite:" + "cajero.db";
+        db.conexionBase(url);
+    }
 }
