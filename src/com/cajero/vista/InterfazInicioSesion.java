ed453695a89938afc0994d8d347017c5d199f529
David <David@192.168.1.16>
2018-06-01 10:38:29 +0100

    Interfaz. Aparezca centrada.

--- src/com/cajero/vista/InterfazInicioSesion.java
+++ src/com/cajero/vista/InterfazInicioSesion.java
@@ -21,6 +21,7 @@ public class InterfazInicioSesion extends javax.swing.JFrame {
      */
     public InterfazInicioSesion() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     /**
