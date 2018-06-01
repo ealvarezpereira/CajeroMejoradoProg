f9f6d3f5ceab14a4edc2141dc38d3a07032aac7b
David <David@192.168.1.16>
2018-06-01 11:37:00 +0100

    insertarDinero

--- src/com/cajero/controlador/Controlador.java
+++ src/com/cajero/controlador/Controlador.java
@@ -27,4 +27,8 @@ public class Controlador {
         objModelo.iniciarSesion(usuario, contr);
     }
     
+    public void insertarDinero(int dinero){
+        objModelo.insertarDinero(dinero);
+    }
+    
 }
